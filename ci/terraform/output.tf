ebca68873c5c000d199453d30f25bf513ccc83bb
dmutreja <dhiraj.mutreja@oracle.com>
2018-04-22 13:48:07 -0700

    Additonal output variables

--- ci/terraform/output.tf
+++ ci/terraform/output.tf
@@ -1,16 +1,17 @@
 output vcn {
   value = "${oci_core_virtual_network.ci_vcn.display_name}"
 }
+
+output vcn_id {
+  value = "${oci_core_virtual_network.ci_vcn.id}"
+}
+
 output subnet_id {
   value = "${oci_core_subnet.director_subnet.id}"
 
 }
 output compartment_id {
    value = "${oci_core_subnet.director_subnet.compartment_id}"
-}
-
-output ad {
-  value = "${oci_core_subnet.director_subnet.availability_domain}"
 }
 
 output subnet_name {
@@ -28,8 +29,21 @@ output subnet_first_ip {
    value = "${cidrhost(oci_core_subnet.director_subnet.cidr_block, 2)}"
 }
 
+output internal_cidr {
+  value = "${oci_core_subnet.director_subnet.cidr_block}"
+}
+output internal_gw {
+  value = "${cidrhost(oci_core_subnet.director_subnet.cidr_block, 1)}"
+}
+output internal_ip {
+  value = "${cidrhost(oci_core_subnet.director_subnet.cidr_block, 2)}"
+}
+
 output bats_subnet1_name {
    value = "${oci_core_subnet.bats_subnet1.display_name}"
+}
+output bats_subnet1_id {
+  value = "${oci_core_subnet.bats_subnet1.id}"
 }
 
 output bats_subnet1_cidr {
@@ -55,6 +69,9 @@ output bats_subnet1_static_ip {
 output bats_subnet2_name {
    value = "${oci_core_subnet.bats_subnet2.display_name}"
 }
+output bats_subnet2_id {
+  value = "${oci_core_subnet.bats_subnet2.id}"
+}
 
 output bats_subnet2_cidr {
    value = "${oci_core_subnet.bats_subnet2.cidr_block}"
@@ -78,4 +95,30 @@ output bats_subnet2_static_ip {
 
 output external_ip {
   value = "${oci_core_public_ip.director_vip.ip_address}"
+}
+
+output tenancy {
+  sensitive = true
+  value = "${var.oracle_tenancy_ocid}"
+}
+output user {
+  sensitive = true
+  value = "${var.oracle_user_ocid}"
+}
+output fingerprint {
+  sensitive = true
+  value = "${var.oracle_fingerprint}"
+}
+output compartment {
+  value = "${oci_core_subnet.director_subnet.compartment_id}"
+}
+output ad {
+  value = "${var.director_ad}"
+}
+output region {
+  value = "${var.oracle_region}"
+}
+output apikey {
+  sensitive = true
+  value = "${file(var.oracle_private_key_path)}"
 }
